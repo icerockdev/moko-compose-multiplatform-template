377960e79edf88cf14941a271d1dca54ebcf9022
Borys Trybitsyn <btrubitsyn@icerockdev.com>
2023-03-20 15:16:27 +0400

    MOKO-1438 fix UI

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,22 +1,32 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 internal fun App() {
     MaterialTheme {
         var currentScreen: Screen by remember { mutableStateOf(Screen.HelloWorld) }
-
-        when (currentScreen) {
-            Screen.HelloWorld -> HelloWorldScreen(
-                onButtonClick = {
-                    currentScreen = Screen.SimpleViewModel
-                }
-            )
-            Screen.SimpleViewModel -> SimpleScreen()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when (currentScreen) {
+                Screen.HelloWorld -> HelloWorldScreen(
+                    onButtonClick = {
+                        currentScreen = Screen.SimpleViewModel
+                    }
+                )
+                Screen.SimpleViewModel -> SimpleScreen()
+            }
         }
     }
 }
