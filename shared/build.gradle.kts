8c7083efb614d624ec9aeb2e9359369532813f73
Borys Trybitsyn <btrubitsyn@icerockdev.com>
2023-03-25 22:13:54 +0400

    MOKO-1438 add App fixes

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -38,7 +38,7 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.material)
                 api("dev.icerock.moko:resources:${mokoResourcesVersion}")
-//                api("dev.icerock.moko:resources-compose:0.20.1")
+                api("dev.icerock.moko:resources-compose:0.21.0")
             }
         }
 
