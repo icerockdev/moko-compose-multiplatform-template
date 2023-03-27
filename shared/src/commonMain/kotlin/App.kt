57c88a85d2eeb402db85c9000452b1a9eb3fc02b
Borys Trybitsyn <btrubitsyn@icerockdev.com>
2023-03-27 16:24:29 +0400

    MOKO-1438 merge main and change screen

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,78 +1,27 @@
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.myapplication.common.MR
-import dev.icerock.moko.resources.compose.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 internal fun App() {
-    //when (currentScreen) {
-    //                Screen.HelloWorld -> HelloWorldScreen(
-    //                    onButtonClick = {
-    //                        currentScreen = Screen.SimpleViewModel
-    //                    }
-    //                )
-    //                Screen.SimpleViewModel -> SimpleScreen()
-    //            }
+    var currentScreen by remember { mutableStateOf(Screen.HelloWorld) }
 
     MaterialTheme(
         colors = if (isSystemInDarkTheme()) darkColors() else lightColors()
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize()
-                .background(color = MaterialTheme.colors.background)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(MR.images.moko_logo),
-                contentDescription = null
+        when (currentScreen) {
+            Screen.HelloWorld -> HelloWorldScreen(
+                onButtonClick = {
+                    currentScreen = Screen.SimpleViewModel
+                }
             )
-
-            var text: String by remember { mutableStateOf("") }
-
-            OutlinedTextField(
-                modifier = Modifier.fillMaxWidth()
-                    .padding(top = 16.dp),
-                value = text,
-                colors = TextFieldDefaults.outlinedTextFieldColors(
-                    textColor = MaterialTheme.colors.onBackground
-                ),
-                onValueChange = { text = it }
-            )
-
-            val counter: Int = text.length
-            Text(
-                modifier = Modifier.fillMaxWidth()
-                    .padding(vertical = 8.dp),
-                text = stringResource(MR.plurals.chars_count, counter, counter),
-                color = colorResource(MR.colors.textColor),
-                fontFamily = fontFamilyResource(MR.fonts.cormorant.italic)
-            )
-
-            Button(onClick = { text = "Hello, ${getPlatformName()}" }) {
-                Text(text = stringResource(MR.strings.hello_world))
-            }
-
-            val fileContent: String? by MR.files.some_file.readTextAsState()
-            Text(
-                modifier = Modifier.padding(top = 16.dp),
-                text = fileContent.orEmpty(),
-                color = MaterialTheme.colors.onBackground
-            )
-
-            val assetContent: String? by MR.assets.some_asset.readTextAsState()
-            Text(
-                modifier = Modifier.padding(top = 16.dp),
-                text = assetContent.orEmpty(),
-                color = MaterialTheme.colors.onBackground
-            )
+            Screen.SimpleViewModel -> SimpleScreen()
         }
     }
 }
